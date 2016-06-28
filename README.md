42c32cbe1e086a1c5a5a0d2ab96d8820f26049b2
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-28 11:01:08 +0200

    Create Messenger in ServiceConnection and set mBound flag

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MessengerServiceKata
 1. Create project
 2. Add ServiceConnection
+3. Create Messenger in ServiceConnection and set mBound flag
--- app/src/main/java/pl/kata/bielanski/messengerservicekata/MainActivity.java
+++ app/src/main/java/pl/kata/bielanski/messengerservicekata/MainActivity.java
@@ -3,10 +3,14 @@ package pl.kata.bielanski.messengerservicekata;
 import android.content.ComponentName;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.os.Messenger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+	private Messenger mService;
+	private boolean mBound;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +21,14 @@ public class MainActivity extends AppCompatActivity {
 	private ServiceConnection mConnection = new ServiceConnection() {
 		@Override
 		public void onServiceConnected(ComponentName name, IBinder service) {
-
+			mService = new Messenger(service);
+			mBound = true;
 		}
 
 		@Override
 		public void onServiceDisconnected(ComponentName name) {
-
+			mService = null;
+			mBound = false;
 		}
 	};
 }
